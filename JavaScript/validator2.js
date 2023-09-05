b87b6e394d64cfb7321750cdcbc1ea503f3db185
kennethtanmh <kennethtanmh99@gmail.com>
2023-09-05 14:55:31 +0800

    updated JS exercises

--- JavaScript/validator2.js
+++ JavaScript/validator2.js
@@ -58,15 +58,15 @@ function chkPhone(event) {
   }
 }
 
-// validator2r.js
-//   The last part of validator2. Registers the
-//   event handlers
-//   Note: This script does not work with IE8
+// // validator2r.js
+// //   The last part of validator2. Registers the
+// //   event handlers
+// //   Note: This script does not work with IE8
 
-// Get the DOM addresses of the elements and register
-//  the event handlers
+// // Get the DOM addresses of the elements and register
+// //  the event handlers
 
-var customerNode = document.getElementById("custName");
-var phoneNode = document.getElementById("phone");
-customerNode.addEventListener("change", chkName, false);
-phoneNode.addEventListener("change", chkPhone, false);
+// var customerNode = document.getElementById("custName");
+// var phoneNode = document.getElementById("phone");
+// customerNode.addEventListener("change", chkName, false);
+// phoneNode.addEventListener("change", chkPhone, false);
